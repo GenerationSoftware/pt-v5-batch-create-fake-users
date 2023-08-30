55de4cc356877e6e774091f11516af64a47b40de
Charles Bergeron <chuck.bergeron@gmail.com>
2023-08-30 14:27:13 -0700

    updated for latest testnet

--- index.ts
+++ index.ts
@@ -1,27 +1,24 @@
 import { ethers, providers } from "ethers";
 import {
+  downloadContractsBlob,
   getSubgraphVaults,
   populateSubgraphVaultAccounts
 } from "@generationsoftware/pt-v5-utils-js";
 
 import { userFakerAbi } from "./abis/userFakerAbi";
 
-const CHAIN_ID = 11155111; // sepolia
+const CHAIN_ID = 420; // opt goerli
+// const CHAIN_ID = 11155111; // sepolia
 
-const USER_FAKER_ADDRESS = "0xb02BB09C774a1eccA01259F68373894f6eFE7164";
-const TOKEN_FAUCET_ADDRESS = "0xcB0A8a7A1d37e35881461a3971148Dd432746401";
+const USER_FAKER_ADDRESS = "0x7506De196cd50f95c53412844743c90B63fE79ef";
+const TOKEN_FAUCET_ADDRESS = "0xc9f91F4aeee31A2a65eC142b2E6c1725Cd160De5";
 
 //
 // NOTE: Make sure to lowercase these addresses so they play nice with the subgraph:
 //
 const SELECTED_VAULTS = [
-  "0xa036647ec8c956475f8b8fe473ec49f959846fa1", //DAI?
-  // "0xc7e0df4a00f9a99012a828dbd2a26dc8c01e624e", //DAI?
-  "0xb634839ac5c7ddcf8523ba7cc2a9211f4f107423" // usdc
-  // "0x2682c2ff5510ff943f2ebfd0a665e3203a9bee4e",
-  // "0xf4236b70bfc155b65a5571a5e4f8961107abfce6",
-  // "0xdc140f0193a9899982c8446c249a94dea147c20b",
-  // "0x80f5984aD748878c9822Af1231dEEF7466Ad85Bf"
+  "0x3bd1ca87f5d5c80b97d57825151715c204444c94", //DAI?
+  "0xfef4a3fe03a480b1872ff0e95a30f0fa16db323d" // usdc
 ];
 
 const getVaults = async (chainId: number) => {
@@ -44,13 +41,17 @@ export async function main() {
   console.log({ TOKEN_FAUCET_ADDRESS });
   console.log("");
 
-  const provider = new providers.JsonRpcProvider(process.env.SEPOLIA_RPC_PROVIDER_URL);
+  const provider = new providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_PROVIDER_URL);
   const privateKey = process.env.PRIVATE_KEY;
   const signer = new ethers.Wallet(privateKey, provider);
+
+  // const tokenAddress = "0x20524C0a56ec85A1aF9b0989202C5bDc1B649Def"; // POOL
+  // await drip(signer, tokenAddress);
 
   const userFaker = new ethers.Contract(USER_FAKER_ADDRESS, userFakerAbi, signer);
 
   let vaults: any = await getVaults(CHAIN_ID);
+  // let vaults: any = [];
 
   if (vaults.length === 0) {
     vaults = [
@@ -64,6 +65,7 @@ export async function main() {
       const vault = vaults[i];
 
       if (!SELECTED_VAULTS.includes(vault.id)) {
+        console.log("skipping");
         continue;
       }
 
@@ -84,8 +86,10 @@ export async function main() {
         TOKEN_FAUCET_ADDRESS,
         { gasLimit: 10000000 }
       );
+      await transactionSentToNetwork.wait();
 
       console.log("TransactionHash:", transactionSentToNetwork.hash);
+      console.log("");
     } catch (error) {
       throw new Error(error);
     }
@@ -105,4 +109,18 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Faucet
+async function drip(signer, tokenAddress) {
+  console.log("drip");
+  const contracts = await downloadContractsBlob(CHAIN_ID);
+
+  const faucetContractData = contracts.contracts.find(contract => contract.type === "TokenFaucet");
+  const faucetContract = new ethers.Contract(TOKEN_FAUCET_ADDRESS, faucetContractData?.abi, signer);
+
+  const tx = await faucetContract.drip(tokenAddress);
+  console.log("tx");
+  console.log(tx.hash);
+  console.log("");
 }
