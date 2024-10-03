971490c5001ad8079640943d3325df0ff85e732e
Charles Bergeron <chuck.bergeron@gmail.com>
2024-10-03 10:06:05 -0700

    add world sepolia

--- index.ts
+++ index.ts
@@ -2,11 +2,9 @@ import yn from "yn";
 import chalk from "chalk";
 import { ethers, providers } from "ethers";
 import {
-  PrizePoolInfo,
   PrizeVault,
   ContractsBlob,
   downloadContractsBlob,
-  getPrizePoolInfo,
   getSubgraphPrizeVaults,
   populateSubgraphPrizeVaultAccounts
 } from "@generationsoftware/pt-v5-utils-js";
@@ -15,6 +13,7 @@ import { userFakerAbi } from "./abis/userFakerAbi";
 import { ERC20Abi } from "./abis/ERC20Abi";
 
 const CHAIN_IDS = {
+  worldSepolia: 4801,
   gnosisChiado: 10200,
   baseSepolia: 84532,
   arbitrumSepolia: 421614,
@@ -24,6 +23,7 @@ const CHAIN_IDS = {
 };
 
 const CHAIN_NAMES = {
+  4801: "worldSepolia",
   10200: "gnosisChiado",
   84532: "baseSepolia",
   421614: "arbitrumSepolia",
@@ -40,6 +40,7 @@ const contractsJsonUrl = process.argv[5];
 const subgraphUrl = process.argv[6];
 
 const USER_FAKER_ADDRESS = {
+  [CHAIN_IDS.worldSepolia]: "0x4b9b0cc30ef739dbaa44457f36bbad8747ad5629",
   [CHAIN_IDS.gnosisChiado]: "0x7e1acab9273afe96571b6c59db669a1b7b914b40",
   [CHAIN_IDS.baseSepolia]: "0x02ac689bb6ad0e07144a520c02e5fe3a3959dfa0",
   [CHAIN_IDS.arbitrumSepolia]: "0xc086edda021d9b90c09bd0092d47c36879c879fb",
@@ -52,6 +53,10 @@ const getProviderUrl = (): string | undefined => {
   let url: string | undefined = "";
 
   switch (selectedChainId) {
+    case CHAIN_IDS.worldSepolia: {
+      url = process.env.WORLD_SEPOLIA_RPC_PROVIDER_URL;
+      break;
+    }
     case CHAIN_IDS.gnosisChiado: {
       url = process.env.GNOSIS_CHIADO_RPC_PROVIDER_URL;
       break;
@@ -151,7 +156,8 @@ export async function main() {
     wethTokenAddress: erc20MintableContracts[0].address.toLowerCase(),
     poolTokenAddress: erc20MintableContracts[1].address.toLowerCase(),
     vault1Address: prizeVaultContracts[0].address.toLowerCase(),
-    vault2Address: prizeVaultContracts[2].address.toLowerCase()
+    vault2Address: prizeVaultContracts[1].address.toLowerCase()
+    // vault2Address: prizeVaultContracts[2].address.toLowerCase()
   };
 
   console.table(addresses);
@@ -161,7 +167,13 @@ export async function main() {
   const signer = new ethers.Wallet(privateKey, provider);
 
   if (onlyDrip) {
-    await drip(contracts, signer, addresses.tokenFaucetAddress, addresses.wethTokenAddress);
+    await drip(
+      contracts,
+      signer,
+      selectedChainId,
+      addresses.tokenFaucetAddress,
+      addresses.wethTokenAddress
+    );
     return;
   }
 
@@ -239,7 +251,12 @@ function delay(ms: number) {
 }
 
 // Faucet
-async function drip(contracts, signer, tokenFaucetAddress, tokenAddress) {
+async function drip(contracts, signer, chainId, tokenFaucetAddress, tokenAddress) {
+  if (chainId === 10200) {
+    console.log("is gnosis chiado, needs wxdai not weth");
+    tokenAddress = "0xb2D0d7aD1D4b2915390Dc7053b9421F735A723E7";
+  }
+
   const tokenContract = new ethers.Contract(tokenAddress, ERC20Abi, signer);
   console.log(`Drip: (${await tokenContract.symbol()}) [${tokenAddress}] to ${signer.address}`);
 
